b50ac4fab0019c8342178d828f31dcc27993f85b
nb <kimnabin01@gmail.com>
2026-08-05 16:04:33 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,17 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="site-header shell">
-      <Link href="/" className="brand">Nabin Kim</Link>
-      <nav>
-        <a href="#work">Work</a>
-        <a href="#beyond">Beyond Work</a>
-      </nav>
+    <header className="site-header">
+      <div className="header-inner">
+        <Link href="/" className="brand">
+          Nabin Kim
+        </Link>
+
+        <nav className="nav">
+          <a href="#work">Work</a>
+          <a href="#beyond">Beyond Work</a>
+        </nav>
+      </div>
     </header>
   );
 }
